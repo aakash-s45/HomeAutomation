c81e32c028741e760aa083dc09de5b52cb7828de
Aakash Solanki <75980718+aakash-s45@users.noreply.github.com>
2021-03-02 21:22:39 +0530

    Update main.cpp

--- SmartHome/src/main.cpp
+++ SmartHome/src/main.cpp
@@ -1,22 +1,40 @@
+// this block is used to include various library
+
 #include <Arduino.h>
-#include <Credentials.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-#include <MQTTCredentials.h>
 
-
+// here all the pins are defined
 #define light1 D1
 #define light2 D2
 #define fan D3
 #define switch D4
 #define light3 D5
 #define sensor D6
+
+// this is the topic from which we recieve the messages
+#define sub1 "esp1/room"
+
+
 int m;        // variable to store motion light state
 int pirValue; // variable to store read PIR Value
+
+//wifi credentials
+const char *ssid = "ssid";
+const char *password = "password";
+
+// mqtt credentials
+const char *mqttServer = "192.168.43.54";
+const int mqttPort = 1883;
+
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+/*
+ this block of code is used to recieve different Messages( payload) from MQTT broker
+ and perform the specific action according to the message recieved
+ */
 void callback(char *topic, byte *payload, unsigned int length) {
     Serial.print("Message arrived in topic: ");
     Serial.print(topic);
@@ -44,7 +62,7 @@ void callback(char *topic, byte *payload, unsigned int length) {
     }
 // For Fan
     if (s.equals(String("ONF"))) {
-        
+        digitalWrite(fan, HIGH);
     } else if(s.equals(String("OFFF"))) {
         digitalWrite(fan, LOW);
     }
@@ -57,20 +75,41 @@ void callback(char *topic, byte *payload, unsigned int length) {
 // For LED
 
     if (s.equals(String("OND"))) {
-        digitalWrite(light3, HIGH);
+        digitalWrite(light3,LOW);
     } else if(s.equals(String("OFFD"))) {
-        digitalWrite(light3, LOW);
+        digitalWrite(light3,HIGH);
     }
-};
+// brightness control for led
+
+    if (s.equals(String("00"))) {
+        analogWrite(light3,1023);
+    } 
+    if (s.equals(String("204"))) {
+        analogWrite(light3,1023-204);
+    } 
+    if (s.equals(String("408"))) {
+        analogWrite(light3,1023-408);
+    } 
+    if (s.equals(String("612"))) {
+        analogWrite(light3,1023-612);
+    } 
+    if (s.equals(String("816"))) {
+        analogWrite(light3,1023-816);
+    } 
+    if (s.equals(String("1020"))) {
+        analogWrite(light3,0);
+    } 
+}
+// connect() function is used to connect to Broker and to subscribe to the topic
 
 void connect() {
     client.setServer(mqttServer, mqttPort);
     client.setCallback(callback);
     while(!client.connected()) {
         Serial.print("Attempting MQTT connection...");
-        if (client.connect("ESP1", mqttUser, mqttPassword)) {
+        if (client.connect("ESP1")) {
             Serial.println("connected");
-            client.subscribe("/****TOPIC*******/");
+            client.subscribe(sub1);
             
         }
         else {
@@ -81,7 +120,7 @@ void connect() {
         }
     }
 };
-
+// in setup() function pinmodes are defined and also used to connect to wifi 
 void setup()
 {
 Serial.begin(115200);
@@ -92,7 +131,7 @@ pinMode(light3, OUTPUT);
 pinMode(fan, OUTPUT);
 pinMode(sensor, INPUT);
 
-digitalWrite(light3, LOW);
+digitalWrite(light3, HIGH);
 
 WiFi.begin(ssid, password);
 
@@ -111,12 +150,19 @@ while (WiFi.status() != WL_CONNECTED)
 
 }
 
-// the loop function runs over and over again forever
+// loop() function here is only used to read the values from PIR sensor and turn led on and off
+// this function also connect function so that connection b/w nodeMCU and Broker does not break
+
 void loop()
 {
   if(m == 1){
     pirValue = digitalRead(sensor);
-  digitalWrite(light3, pirValue);
+    if(pirValue == HIGH){
+        digitalWrite(light3, LOW);
+    }
+    else{
+       digitalWrite(light3, HIGH); 
+    }
   }
 
      if (!client.connected()){
